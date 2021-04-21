c4c13f61d9ecce48784d9a634b69a102d51537fa
jonschull <Jschull@gmail.com>
2021-04-21 13:03:07 -0400

    removed errorbox.py

--- errorBox.py
+++
@@ -1,7 +0,0 @@
-
-### error box for print()
-class MyOutput:
-    def __init__(self):
-        self.console = document["console"]
-    def write(self, text):
-        self.console.html +=  text + '</hr>'
